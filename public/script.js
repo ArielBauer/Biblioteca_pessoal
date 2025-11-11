e62f88b2f0a6fb2c3914a9265bf69ddb77d5b1b1
LaylaPrates <laylaprates.21@gmail.com>
2025-11-11 11:46:25 -0300

    atualização do index e script, para mostrar as informações dos livros já salvos

--- public/script.js
+++ public/script.js
@@ -1,21 +1,28 @@
 
-function carregaDados () {
+let lista = document.getElementById("lista");
 
-let url = `http://localhost:3000/livros`;
+// Função para carregar livros
+async function carregarLivros() {
+  const resposta = await fetch("/livros");
+  const livros = await resposta.json();
+  console.log(livros);
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-
-    .then((data) => {
-      console.log(data)
-    })
-    
-    .catch((error) => {
-      console.log("erro no carregamento de dados");
-    });
-
+  //Adiciona livros já cadastrados a lista
+  livros.forEach((u) => {
+    const li = document.createElement("li");
+    li.textContent = u.titulo;
+    lista.appendChild(li);
+    const ul = document.createElement("ul");
+    ul.textContent = " "
+    li.appendChild(ul);
+    const li2 = document.createElement("li");
+    li2.textContent = `Autor(a): ${u.autor}`;
+    ul.appendChild(li2);
+    const li3 = document.createElement("li");
+    li3.textContent = `Total de páginas: ${u.paginas_total}`;
+    ul.appendChild(li3);
+  });;
 };
 
-carregaDados();
+// Carrega ao abrir a página
+carregarLivros();
